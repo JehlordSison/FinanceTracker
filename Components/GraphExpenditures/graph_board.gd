36e243b2d0133b057030a6455068686616d9ff9a
Jeh <62175936+JehlordSison@users.noreply.github.com>
2025-07-12 15:39:49 +0800

    Update

--- Components/GraphExpenditures/graph_board.gd
+++ Components/GraphExpenditures/graph_board.gd
@@ -1,4 +1,15 @@
 extends Panel
 	
+var default_size: Vector2
+
+func _ready():
+	default_size = size
+
+func _on_full_screen_btn_pressed():
+	if size == default_size:
+		size = get_viewport_rect().size
+	else:
+		size = default_size
+
 func _on_add_transaction_btn_pressed():
-	Instances.add_transcation_panel(get_parent())
+	Instances.add_transcation_panel($"../../../..")
